d2db73fb5b45612f8875c81b7a5a17c1c7efcea6
Christian <114274872+CristianLaimon@users.noreply.github.com>
2024-06-18 10:55:06 -0600

    Fixed a bug with undefined command modules

--- source/Bot/command_center.ts
+++ source/Bot/command_center.ts
@@ -10,7 +10,7 @@ const commandsToAdd: ((bot: PermaTecBot) => void)[] = [];
 /** Array containing all frontend inline buttons events for INLINE KEYBOARD API */
 const eventsToAdd: ((bot: PermaTecBot) => void)[] = [];
 
-/** Reads all command files RECURSIVELY on Command Folder from config.json */ //Local Method
+/** Reads all command files RECURSIVELY on command folder name from from config.json */ //Local Method
 function loadCommandsAndEvents(dir: string) {
   fs.readdirSync(dir).forEach(file => {
     const fullPath = path.join(dir, file);
@@ -19,11 +19,11 @@ function loadCommandsAndEvents(dir: string) {
     } else if (file.endsWith(".js")) {
       try {
         const commandModule: CommandModule = require(fullPath);
-        commandsToAdd.push(commandModule.command);
+        if (typeof commandModule.command !== "undefined")
+          commandsToAdd.push(commandModule.command);
 
-        if (typeof commandModule.events !== "undefined") {
+        if (typeof commandModule.events !== "undefined")
           eventsToAdd.push(commandModule.events);
-        }
       } catch (err) {
         console.error(`Error loading module ${fullPath}:`, err);
       }
